b0cff52c4d3e0de77459072ab5c9982b1dc072cc
MIchaelSciole798 <81838472+MIchaelSciole798@users.noreply.github.com>
2021-11-26 11:29:36 -0500

    test

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require("express");
+import express from "express";
 const app = express();
+import caramels from "./caramels.json";
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello there!"));
+app.get("/", (req, res) => res.send(caramels));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
